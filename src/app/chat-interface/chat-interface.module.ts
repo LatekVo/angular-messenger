431d6550ff7b7233182c08bc8b6643f573657ae6
LatekVon <latkaignacy@gmail.com>
2023-03-15 03:31:39 +0100

    - added basic friend list along with friend filtering functionality - added 'lodash' external dependency, being used for deepClone()

--- src/app/chat-interface/chat-interface.module.ts
+++ src/app/chat-interface/chat-interface.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FriendsListComponent } from './friends-list/friends-list.component';
+import {FriendFilterPipe, FriendsListComponent} from './friends-list/friends-list.component';
 import { TextAreaComponent } from './text-area/text-area.component';
 import { InfoTabComponent } from './info-tab/info-tab.component';
 import { ChatInterfaceComponent } from './chat-interface.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     FriendsListComponent,
     TextAreaComponent,
     InfoTabComponent,
-    ChatInterfaceComponent
+    ChatInterfaceComponent,
+
+    // custom utilities
+    FriendFilterPipe
   ],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
   exports: [
     ChatInterfaceComponent
   ],
--- src/app/chat-interface/friends-list/friends-list.component.ts
+++ src/app/chat-interface/friends-list/friends-list.component.ts
@@ -1,30 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import {Friend} from "../../models/friend";
 import {HttpClient} from "@angular/common/http";
+import {catchError, throwError} from "rxjs";
+import { cloneDeep } from 'lodash'; // it's absurd deepCloning isn't built in
 
 @Component({
   selector: 'app-friends-list',
   templateUrl: './friends-list.component.html',
   styleUrls: ['./friends-list.component.css']
 })
-export class FriendsListComponent implements OnInit{
-  friendList: Friend[] = [
-    // placeholder values for testing
-    {firstName: "Bob", lastName: "Drew", pfpSourceUrl: "/assets/placeholder_pfp.png"},
-    {firstName: "Ale", lastName: "Moin", pfpSourceUrl: "/assets/placeholder_pfp.png"},
-    {firstName: "Lew", lastName: "Berg", pfpSourceUrl: "/assets/placeholder_pfp.png"}
-  ];
+export class FriendsListComponent implements OnInit {
+
+  friendList: Friend[] = []
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.fetchFriends().subscribe((friendList) => {
       this.friendList = friendList;
-    });
+    })
+
+    // fixme: temporarily hardcoded
+    this.friendList = [
+      // placeholder values for testing
+      {firstName: "Bob", lastName: "Drew", pfpSourceUrl: "/assets/placeholder_pfp.png"},
+      {firstName: "Ale", lastName: "Moin", pfpSourceUrl: "/assets/placeholder_pfp.png"},
+      {firstName: "Lew", lastName: "Berg", pfpSourceUrl: "/assets/placeholder_pfp.png"}
+    ]
   }
+
+  // TODO: add API for fetchFriendList
 
   fetchFriends(): Observable<Friend[]> {
     return this.http.get<Friend[]>('/api/fetchFriendList');
+  }
+
+  // we are using the ngModel to bind the input box to this var
+  searchInput: string = "";
+  onSearchBoxChange() {
+
+  }
+}
+
+// search box custom filter
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'friendFilter',
+  pure: false // setting pure to false enables the pipe to re-run on every change detection cycle
+})
+export class FriendFilterPipe implements PipeTransform {
+  transform(friends: Friend[], searchText: string): Friend[] {
+    if (!friends || !searchText) {
+      return friends;
+    }
+
+    searchText = searchText.toLowerCase();
+    return friends.filter(friend =>
+      friend.firstName.toLowerCase().includes(searchText) ||
+      friend.lastName.toLowerCase().includes(searchText)
+    );
   }
 }
