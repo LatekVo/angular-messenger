50e34e70a5c55465b779357719bef5ae17beb253
LatekVon <latkaignacy@gmail.com>
2023-03-20 22:20:28 +0100

    - added node.js server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const port = process.env.NODE_PORT || 3000;
+
+const rootPath = path.join(__dirname, 'dist', 'angular-messenger');
+
+// !!! before launching, remember to ng build, dist doesn't exist without building first, and it's not included in the GitHub repo
+
+// simply setting a static path won't work, due to how angular is compiled
+app.get('*', function(req, res) {
+  fs.stat(rootPath + req.path, function(err) {
+    if (err) {
+      res.sendFile("index.html", { root: rootPath });
+    } else {
+      res.sendFile(req.path, { root: rootPath });
+    }
+  })
+});
+
+app.listen(port);
+console.log('Listening on port ' + port);
