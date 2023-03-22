1fd032a798ef24b728ffa757638edf2920937c6a
LatekVon <latkaignacy@gmail.com>
2023-03-22 22:31:43 +0100

    - added basic register & login website - other minor changes

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class SignInComponent {
 
+  showLoginForm: boolean = true;
+
+  // both registering and logging in will be executed on the same page, I will use ngIf to switch between the two actions
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      passwordRepeat: new FormControl('', [Validators.required, Validators.minLength(8)])
+
+    });
+
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submitRegisterForm() {
+
+  }
 }
