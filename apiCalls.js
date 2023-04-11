deccf2caabbb22937327273a18c5bbc127ae78b0
LatekVon <latkaignacy@gmail.com>
2023-04-11 21:42:52 +0200

    - finished /login logic - finished /register logic - added Date() utility functions - modified 'tokens' table

--- apiCalls.js
+++ apiCalls.js
@@ -10,6 +10,16 @@ const PAGE_URL = 'localhost:3000'
 
 let hashCost = 12; // todo: this value can be changed without harming anything, but should get tuned for ~150ms per hash comparison
 
+// converts SQL date string to Date object
+function stringToDate(string) {
+  return new Date(Date.parse(string))
+}
+
+// converts ISO date to SQL date
+function dateToString(date) {
+  return date.toISOString().slice(0, 19).replace('T', ' ')
+}
+
 // db init will be moved to server.js later, but for now it's only being used here
 // datetime format: yyyy-MM-dd HH:mm:ss ie: '2007-01-01 10:00:00'
 // to get this time format in js: date.toISOString().slice(0, 19).replace('T', ' '); date being the Date() std class
@@ -25,7 +35,7 @@ CREATE TABLE IF NOT EXISTS tokens (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   userId INTEGER,
   token TEXT,
-  created DATETIME,
+  expiry DATETIME,
 
   FOREIGN KEY(userId) REFERENCES users(id)
 
@@ -63,21 +73,69 @@ router.post('/tokenLogin', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+  console.log("req body:")
+  console.log(req.body)
+
   let username = req.body['username'],
       password = req.body['password']
 
-  console.log('b')
-
   // todo: settle on some specific syntax for those calls, this one is all over the place
-  db.get(`SELECT password, id FROM users WHERE username=?;`, [username], (err, output) => {
+  db.get(`SELECT password, id FROM users WHERE username=?;`, [username], (err, user) => {
     if (err) {
       console.log('--- SQLITE ERROR ---')
       console.log(err)
       return
     }
 
-    console.log(output)
-  })
+    let storedPasswordHash = user.password,
+        storedUserId = user.id
+
+    let isEqual = bcrypt.compareSync(password, storedPasswordHash)
+
+    if (isEqual) {
+      // make sure the record has been deleted, then proceed with creating the new one
+      db.run(`DELETE FROM tokens WHERE userId=?`, [storedUserId], (err) => {
+        if (err) {
+          // this error could just be a 'not found' message, this is to be expected since we are executing the deletion without checking if the record exists.
+          console.log('--- SQL INFORMATION ---')
+          console.log(err)
+        }
+
+        // ~4.5 * 10^15 combinations
+        let hashGen = () => {
+          return Math.random().toString(36).substring(2)
+        }
+
+        let tokenHash = hashGen() + hashGen(),
+            tokenUserId = storedUserId,
+            tokenExpiryDate = new Date(); // unused for now, but necessary for later
+
+        let currentDate = new Date();
+        tokenExpiryDate.setDate(tokenExpiryDate.getDate()+31); // set the expiry as 1 month into the future
+
+        let tokenExpiryDateString = dateToString(tokenExpiryDate)
+
+        db.run(`INSERT INTO tokens (userId, token, expiry) VALUES (?, ?, ?);`, [tokenUserId, tokenHash, tokenExpiryDateString], (err) => {
+          if (err) {
+            // this error could just be a 'not found' message, this is to be expected since we are executing the deletion without checking if the record exists.
+            console.log('--- SQL ERROR ---')
+            console.log(err)
+          } else {
+            // reminder to self before I forget again - we won't be validating an already existing token, GET /tokenLogin already does that
+            // send session token & home page
+            res.writeHead(200)
+            res.cookie('sessionToken', tokenHash)
+            res.send()
+          }
+        })
+      })
+    } else {
+      // wrong credentials - ask to retry
+      res.writeHead(401)
+      res.send()
+    }
+
+  });
 });
 
 router.post('/register', (req, res) => {
@@ -86,15 +144,16 @@ router.post('/register', (req, res) => {
       password = req.body['password']
 
   // todo: add verification etc, we don't want people spamming empty accounts
+  let passwordHash = bcrypt.hashSync(password)
 
-  db.run(`INSERT INTO users (email, username, password) VALUES (?, ?, ?);`, [email, username, passwordHash], (err, output) => {
+  db.run(`INSERT INTO users (email, username, password) VALUES (?, ?, ?);`, [email, username, passwordHash], (err) => {
     if (err) {
       console.log('--- SQLITE ERROR ---')
       console.log(err)
-      return
+    } else {
+      res.writeHead(200)
+      res.send()
     }
-
-    console.log(output)
   })
 });
 
