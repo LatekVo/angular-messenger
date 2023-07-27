64756a4559eed7089e156807b991dc53b537096e
LatekVon <latkaignacy@gmail.com>
2023-07-27 23:51:41 +0200

    - urgent quick fix, those 2 'unused' lines were actually crucial for the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { SignInComponent } from "./sign-in/sign-in.component";
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// vvv THESE TWO LINES ARE 'unused' BUT DELETING THEM WILL BREAK THE PROJECT.
+import { ChatInterfaceModule } from "./chat-interface/chat-interface.module";
+import { SignInModule } from "./sign-in/sign-in.module";
+
 const routes: Routes = [
   { path: 'login', component: SignInComponent },
   { path: 'chat', component: ChatInterfaceComponent },
@@ -24,6 +28,10 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes),
     BrowserAnimationsModule,
+
+    // these two are unnecessary, but I added them to mark the aforementioned two lines marked as used.
+    ChatInterfaceModule,
+    SignInModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { PersonNameOrSurnamePipe } from './pipes/person-name-or-surname.pipe';
 import { FormsModule } from "@angular/forms";
 import { NotificationSnackBarComponent } from './components/notification-snack-bar/notification-snack-bar.component';
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatSnackBarModule
   ],
   declarations: [
     PersonNameOrSurnamePipe,
